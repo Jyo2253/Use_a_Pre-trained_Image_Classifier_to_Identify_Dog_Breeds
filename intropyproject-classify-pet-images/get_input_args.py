bbe131827d9ee779cceea06bf1972a8ba46ff8fd
Mamillapalli Jyothika <109164392+Jyo2253@users.noreply.github.com>
2023-07-10 21:14:58 +0530

    Update get_input_args.py

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
+# PROGRAMMER: M.Jyothika
+# DATE CREATED:  7/10/2023                                
 # REVISED DATE: 
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
@@ -38,6 +38,12 @@ def get_input_args():
     Returns:
      parse_args() -data structure that stores the command line arguments object  
     """
-    # Replace None with parser.parse_args() parsed argument collection that 
+    # Replace None with parser.parse_args() parsed argument collection that
     # you created with this function 
-    return None
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--arch', default = 'vgg' )
+    parser.add_argument('--dogfile', default = 'dognames.txt' )
+    
+    
+    
+    return parser.parse_args()
